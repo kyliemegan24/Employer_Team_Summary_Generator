094b9fdc699ed62ab5117e727f1fe15642dea1bd
Kylie S <kyliemegan24@gmail.com>
2020-10-19 17:45:43 -0500

    Wrote code for init function that will run questions, wrote code that will push employees into array, wrote the renderOutput function that writes the team.html file

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
 const questions = [
     {
@@ -69,22 +68,46 @@ const questions = [
     }
 ];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+async function init() {
+	
+	try {
+		const answers = await inquirer.prompt(questions);
+                const { name, id, email, role } = answers;
+		switch (role) {
+			case 'Manager':
+				let manager = new Manager(name, id, email, answers.officeNumber);
+				employees.push(manager);
+				break;
+			case 'Engineer':
+				let engineer = new Engineer(name, id, email, answers.github);
+				employees.push(engineer);
+				break;
+			case 'Intern':
+				let intern = new Intern(name, id, email, answers.school);
+				employees.push(intern);
+		}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    		
+   		if (answers.addMore) init();
+    
+   		 
+    		renderOutput();
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+
+function renderOutput() {
+  if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR);
+  const teamHTML = fs.writeFileSync(outputPath, render(employees), (err) => {
+     if (err) throw err;
+  } )
+}
+
+
+init();
+
+
